49c13325425a3d6a42187d5b01bae823819bf6fb
Ryan See <ryanjosephsee@gmail.com>
2022-10-31 15:01:38 -0700

    commit tree a9a66f34cbc0d094f06aa28bd4e3ccb6cc5ec67b

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Code your solution in this file!
+const headquarters = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks (num){
+
+    if (num > 42){
+    return num - headquarters;
+    }else if (num < 42){
+        return headquarters - num;
+    }
+
+}
+
+function distanceFromHqInFeet (num){
+    return distanceFromHqInBlocks(num) * feet;
+    
+}
+
+function distanceTravelledInFeet (num){
+    return distanceFromHqInBlocks(num) ;
+}
+
+function distanceTravelledInFeet (headquarters, num2){
+    if (num2 > headquarters){
+        return (num2 - headquarters)*feet;
+
+    }else{
+    return (headquarters - num2)*feet;
+    }
+}
+
+function calculatesFarePrice(num, headquarters){
+    const distance = Math.abs(num, head) * 264;
+    if (distance <= 400) {
+        return 0;
+    }  else if (distance <= 2000) {
+        return (distance - 400) *.02;
+    } else if (distance <= 2500) {
+        return 25;
+    } else if (distance > 2501) {
+        return 'cannot travel that far';
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (start-destination <= 400) {
+    return 0;
+    }  else if (start-destination <= 2000) {
+    return (start-destination - 400) *.02;
+    } else if (start-destination <= 2500) {
+    return 25;
+    } else if (start-destination > 2501) {
+    return 'cannot travel that far';
+    }
+}
